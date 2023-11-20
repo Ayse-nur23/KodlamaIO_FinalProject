52fc9fe332d8bd5509bd78f124e0676ddb6bf180
Ayşenur Elmas <ayse66025@gmail.com>
2023-11-20 23:37:21 +0300

    Day_8.1

--- DataAccess/Concretes/EntityFramework/EfCategoryDal.cs
+++ DataAccess/Concretes/EntityFramework/EfCategoryDal.cs
@@ -1,13 +1,13 @@
-﻿using Entities.Concretes;
+﻿using DataAccess.Abstract;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DataAccess.Abstract
+namespace DataAccess.Concretes.EntityFramework
 {
-    public interface IProductDal : IEntityRepository<Product>
+    public class EfCategoryDal : ICategoryDal
     {
     }
 }
